6ee1eed0245c5baadf2227a128e97d386d649736
charley <diamonddynamo96@gmail.com>
2018-05-02 14:15:47 -0400

    Both browse buttons now work

--- app/src/main/java/com/i4i/hcvb/route26/ProducerDashboardActivity.java
+++ app/src/main/java/com/i4i/hcvb/route26/ProducerDashboardActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class ProducerDashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,6 +35,15 @@ public class ProducerDashboardActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button browseButton = findViewById(R.id.pro_browse);
+        browseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
